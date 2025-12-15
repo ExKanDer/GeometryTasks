4d3601010d179dedc81a843e12df4c64d300db14
ExKanDer <david.shirikyan@yandex.ru>
2025-12-15 22:20:48 +0300

    Delete PlaneGeometry/geometry.h

--- PlaneGeometry/geometry.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "point.h"
-
-/// Возвращает:
-///  1  - точка слева от направленного отрезка AB
-/// -1  - справа
-///  0  - лежит на отрезке (коллинеарна и между A и B)
-int pointRelativeToSegment(const Point& A, const Point& B, const Point& P);
